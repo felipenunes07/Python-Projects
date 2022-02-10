b29d6a3b702b5c375b0fcee5680bb9f6f4940327
Felipe Arian Galeão Nunes <94389838+felipenunes07@users.noreply.github.com>
2022-02-09 22:10:49 -0300

    Phonetic_alphabet

--- Project11 - Phonetic_alphabet/Phonetic_alphabet.py
+++ Project11 - Phonetic_alphabet/Phonetic_alphabet.py
@@ -3,6 +3,7 @@
 
 import pandas
 
+
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 #TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
